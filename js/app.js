178e0443a342424cdfff3057a9ddfab75b6e8f04
Yukyung Seo <syk332@naver.com>
2022-05-25 10:00:22 +0900

    modify skeleton

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// MainContoller와 연결을 한다.
-import MainContoller from "./controllers/MainController.js"
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("app.js");
-    MainContoller.init();
-});
--- js/views/DeckView.js
+++
@@ -1,5 +0,0 @@
-
-    // 유저 카드는 다 보여주기
-    displayUserCard();
-    // 딜러 카드 보여주기
-    displayDillerCard();
